8461e339006af5968a82ad90de579afde00e37f8
Mou Deb <94340444+mou58@users.noreply.github.com>
2022-10-31 21:41:05 -0500

    Create unet2d.py

---
+++ tensorflow/unet2d/unet2d.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dropout, Activation
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Conv2DTranspose
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.models import Model
+import numpy as np
+
+
