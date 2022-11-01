cd070564de89b74cd241a8e9670f6e94defa6fba
Mou Deb <94340444+mou58@users.noreply.github.com>
2022-10-31 21:41:51 -0500

    Update unet2d.py

--- tensorflow/unet2d/unet2d.py
+++ tensorflow/unet2d/unet2d.py
@@ -5,4 +5,13 @@ from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.models import Model
 import numpy as np
 
+#%% Parameters
+INPUT_SIZE = 64 # Input feature width/height
+INPUT_CHANNEL = 1
+OUTPUT_SIZE = 64 # Output feature width/height 
+OUTPUT_CHANNEL = 1
+OUTPUT_CLASSES = 1 # Number of output classes in dataset
+base_filt = 32
+dropout_rate = 0.15
+
 
